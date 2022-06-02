7bf473d3f27e51047360341bd48d2f04f386d14e
EngKhallo <engkhalid.ict@gmail.com>
2022-06-02 17:09:41 +0300

    New - Income Model

---
+++ Models/Income.cs
@@ -0,0 +1,16 @@
+namespace Income_Expense_Manager.Models
+{
+    public class Income
+    {
+        public int Id { get; set; }
+        public string IncomeName { get; set; }="";
+        public int IncomeCategoryId { get; set; }
+        public IncomeCategory? IncomeCategory { get; set; }
+        public int AccountId { get; set; }
+        public Account? Account { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string? Description { get; set; }
+
+    }
+}
