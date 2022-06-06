cb784f9f05a65f3ed7f7a87e2cae1964e48aae9c
EngKhallo <engkhalid.ict@gmail.com>
2022-06-06 15:38:46 +0300

    Update - IncomeViewModel

--- ViewModels/IncomeViewModel.cs
+++ ViewModels/IncomeViewModel.cs
@@ -3,9 +3,10 @@ using Income_Expense_Manager.Models;
 namespace Income_Expense_Manager.ViewModels;
 public class IncomeViewModel
 {
+    public int Id { get; set; }
     public string IncomeName { get; set; } = "";
-    public IncomeCategory? IncomeCategory { get; set; }
-    public Account? Account { get; set; }
+    public int IncomeCategoryId { get; set; }
+    public int AccountId { get; set; }
     public decimal Amount { get; set; }
     public DateTime CreatedAt { get; set; }
     public string? Description { get; set; }
