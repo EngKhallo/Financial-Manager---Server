5e5be06089edfd42e4a2344749e56287d05ce2f3
EngKhallo <engkhalid.ict@gmail.com>
2022-06-10 01:34:38 +0300

    Update : Responses

--- ViewModels/Response.cs
+++ ViewModels/Response.cs
@@ -1,12 +1,26 @@
+using Income_Expense_Manager.Models;
+
 namespace Income_Expense_Manager.ViewModels;
 
 public class Response<T>
 {
     private List<IncomeCategoryViewModel> category;
+    private List<Income> incomes;
+    private List<Expense> expenses;
 
     public Response(T data)
     {
         Data = data;
+    }
+
+    public Response(List<Income> incomes)
+    {
+        this.incomes = incomes;
+    }
+
+    public Response(List<Expense> expenses)
+    {
+        this.expenses = expenses;
     }
 
     public T Data { get; set; }
