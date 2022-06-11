708d41fcd8005fb49e72921744736fc94049f9a6
EngKhallo <engkhalid.ict@gmail.com>
2022-06-11 09:07:55 +0300

    Update - Added IncomeCategory : IncomeCategory Model

--- Models/Income.cs
+++ Models/Income.cs
@@ -5,7 +5,7 @@ namespace Income_Expense_Manager.Models
         public int Id { get; set; }
         public string IncomeName { get; set; }="";
         public int IncomeCategoryId { get; set; }
-        public IncomeCategory? IncomeCategory { get; set; }
+        public IncomeCategory IncomeCategory { get; set; } = null!;
         public int AccountId { get; set; }
         public Account? Account { get; set; }
         public decimal Amount { get; set; }
